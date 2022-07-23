9244afd5f415ff9c112a1b5619ce2b0348c780ef
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-07-23 13:16:43 +0530

    Create 1587.sql

---
+++ 1587.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+select name, balance
+from (select account, sum(amount) as balance
+    from Transactions
+    group by account
+    having sum(amount) > 10000) t
+join Users u
+on u.account = t.account;
+
