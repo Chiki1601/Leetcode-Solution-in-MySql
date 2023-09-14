5faea0f74a4955f601da262584094a01fdd494f6
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-09-14 11:31:32 +0530

    Create 1211.sql

---
+++ 1-3000/1211.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT query_name ,ROUND(AVG(rating / position) , 2) AS quality ,
+ROUND(SUM(IF(rating<3,1,0))/COUNT(1) * 100,2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name
